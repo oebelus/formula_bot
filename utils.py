409a974e0338a7015fbf567abc508dc7a9f220a5
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-03-21 01:49:41 +0000

    Fixing a smol bug

--- utils.py
+++ utils.py
@@ -11,20 +11,20 @@ f_one_data = json.load(f_one)
 
 def get_fe():
     count_f_e = 0
+    text = "No Upcoming Formula E Races!"
     for data in f_e_data:
         if int(f_e_data[data]['start_date']) <= (now.day + 8)%31 + 1 and f_e_data[data]['month'] == now.strftime("%b") and int(f_e_data[data]['start_date']) == now.day: 
             round = data
             month = f_e_data[data]['month']
             day = f_e_data[data]['start_date']
             location = f_e_data[data]['location']
-            
             text = f'🏎️ Formula E Alert! 🏁\n----\nReminder: {round} of the Formula E season is scheduled to start on {month} {day} in {location}.'
-            return text
         count_f_e += 1
-    return "No Upcoming Formula E Races!"
+    return text
 
 def get_fone():
     count_f_one = 0
+    text =  "No Upcoming Formula 1 Races!"
     for data in f_one_data:
         if int(f_one_data[data]['start_date']) <= (now.day + 8)%31 + 1 and f_one_data[data]['month'].split("-")[0] == now.strftime("%b"): 
             title = f_one_data[data]['event_title']
@@ -33,6 +33,5 @@ def get_fone():
             month = f_one_data[data]['month']
             location = f_one_data[data]['location']
             text = f'🏎️ Formula 1 Alert! 🏁\n---\nReminder: {title} is scheduled for {start} to {end} {month} in {location}.'
-            return text
         count_f_one += 1
-    return "No Upcoming Formula 1 Races!"
+    return text
