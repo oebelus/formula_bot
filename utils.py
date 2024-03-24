c6fc524307f07fd8d1c1c48b3153295f7e747d71
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-03-24 23:53:59 +0000

    typo

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ f_one_data = json.load(f_one)
 
 def get_fe():
     count_f_e = 0
-    text = "No Upcoming Formula E Races!"
+    text = "No Upcoming Formula E Races This Week!"
     for data in f_e_data:
         if int(f_e_data[data]['start_date']) <= (now.day + 8)%31 + 1 and f_e_data[data]['month'] == now.strftime("%b") and int(f_e_data[data]['start_date']) == now.day: 
             round = data
@@ -24,7 +24,7 @@ def get_fe():
 
 def get_fone():
     count_f_one = 0
-    text =  "No Upcoming Formula 1 Races!"
+    text =  "No Upcoming Formula 1 Races This Week!"
     for data in f_one_data:
         if int(f_one_data[data]['start_date']) <= (now.day + 8)%31 + 1 and f_one_data[data]['month'].split("-")[0] == now.strftime("%b"): 
             title = f_one_data[data]['event_title']
